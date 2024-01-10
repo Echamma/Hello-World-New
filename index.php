8d414c030f5fa61972e9a6c1165e10468c2aac9f
Echamma <151067103+Echamma@users.noreply.github.com>
2024-01-09 19:35:29 -0600

    message php

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
+<?php
+$name = "Myname";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello BROZAR</title>
+    <title>Hello BROZAR ISabella</title>
 </head>
 <body>
-    <h1>Heloo Bestie</h1>
+    <h1>Heloo Bestie<?php echo $name?></h1>
 </body>
 </html>
